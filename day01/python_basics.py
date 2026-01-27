e1bf3fec830f4840ff846952c011492c35aae41f
fatihozturk-ai <fatihozt3@hotmail.com>
2026-01-27 19:31:26 +0300

    day01 python basics working

---
+++ day01/python_basics.py
@@ -0,0 +1,50 @@
+# Değişkenler
+name = "Fatih"
+age = 24
+height = 1.75
+is_student = True 
+
+print(name)
+print(age)
+print(is_student)
+
+# Basit matematik
+x=5
+y=6
+
+print(x+y)
+print(x/y)
+print(x*y)
+
+#If/Else
+
+if age > 18:
+	print("Reşit")
+else:
+	print("Reşit Değil")
+
+# Listeler
+
+numbers =  [1,2,3,4,5]
+print(numbers)
+print(numbers[0])
+print(numbers[-1])
+
+# For Loop
+
+for i in numbers:
+	print(i * 2)
+
+# Fonksiyon
+
+def square(number):
+	return number*number
+
+
+result = square(5)
+print(result)
+
+
+
+
+
