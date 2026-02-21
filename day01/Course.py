d102c0db633552ced9bd00122569a5c650e4213f
fatihozturk-ai <fatihozt3@hotmail.com>
2026-02-21 22:48:46 +0300

    python basics started fast

---
+++ day01/Course.py
@@ -0,0 +1,15 @@
+name = "Micheal Jackson"
+
+print(name[0:5:2])
+print(name[::2])
+
+
+print(len(name))
+
+print("Micheal \n Jackson")
+print("Micheal \t Jackson")
+
+nameSec = name.upper()
+print(nameSec)
+nameSec = name.replace("Jackson","Ozturk")
+print(nameSec)
